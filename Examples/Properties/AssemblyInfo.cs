36d947ed17d0b08762d2094c23af7bbeb7bf9622
Chris Miller <chrismiller7@gmail.com>
2017-01-23 11:26:39 -0500

    Fix assembly info

--- Examples/Properties/AssemblyInfo.cs
+++ Examples/Properties/AssemblyInfo.cs
@@ -5,8 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("GCodeNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
